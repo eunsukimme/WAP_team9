6fc01260fb54fdb1bd2cda5e0d021ec2c98674a2
sanghun219 <45305498+sanghun219@users.noreply.github.com>
2019-01-11 21:09:34 +0900

    Update note
    주석달기

--- git_seminar/WAP_9/WAP_9/GameScene.h
+++ git_seminar/WAP_9/WAP_9/GameScene.h
@@ -1,13 +1,13 @@
-#pragma once
+#pragma once // 헤더 가드, 헤더 파일의 중복을 방지시킨다. 클래스 생성시 헤더파일에 자동으로 생성됨
 class GameScene
 {
 public:
-	virtual void Init() =0;
+	virtual void Init() =0; // 순수 가상함수 , 정의는 됐지만 구현은 자식 오브젝트에게 미룬다.
 	virtual void Update() =0;
 	virtual void Draw() =0;
 	virtual void Destroy()=0;
 public:
 	GameScene();
-	virtual ~GameScene();
+	virtual ~GameScene(); // 소멸자에 virtual을 붙여주지않으면 자식 오브젝트의 소멸자는 절대로 호출될 수 없다.
 };
 
